cc59885204b21c8554a8014c7561316151c26919
NymWidana <widan0001@gmail.com>
2023-08-18 20:47:15 +0800

    first commit

---
+++ src/components/Shop.vue
@@ -0,0 +1,304 @@
+<script setup>
+import { ref } from "vue";
+const navs = [
+  {
+    teks: "HOME",
+    url: "/",
+  },
+  {
+    teks: "SHOP",
+    url: "/Shop",
+  },
+  {
+    teks: "TIPS",
+    url: "/Tips",
+  },
+];
+
+function myFunction() {
+  var element = document.getElementById("hamBtn");
+  element.classList.toggle("close");
+  var element = document.getElementById("nav");
+  element.classList.toggle("open");
+}
+
+const products = [
+  {
+    name: "Sayur lokal / local vegies",
+    url: "./src/assets/wortel.png",
+    category: "vegetables",
+    price: 12850,
+  },
+  {
+    name: "buah lokal / local fruits",
+    url: "./src/assets/wortel.png",
+    category: "fruits",
+    price: 12850,
+  },
+  {
+    name: "daging lokal / local meats",
+    url: "./src/assets/wortel.png",
+    category: "meats",
+    price: 12850,
+  },
+  {
+    name: "obat lokal / local herbs",
+    url: "./src/assets/wortel.png",
+    category: "herbs",
+    price: 12850,
+  },
+  {
+    name: "seafood lokal / local seafoods",
+    url: "./src/assets/wortel.png",
+    category: "seafoods",
+    price: 12850,
+  },
+  {
+    name: "Wortel lokal / local carrot",
+    url: "./src/assets/wortel.png",
+    category: "others",
+    price: 12850,
+  },
+  {
+    name: "Sayur lokal / local vegies",
+    url: "./src/assets/wortel.png",
+    category: "vegetables",
+    price: 12850,
+  },
+  {
+    name: "buah lokal / local fruits",
+    url: "./src/assets/wortel.png",
+    category: "fruits",
+    price: 12850,
+  },
+  {
+    name: "daging lokal / local meats",
+    url: "./src/assets/wortel.png",
+    category: "meats",
+    price: 12850,
+  },
+  {
+    name: "obat lokal / local herbs",
+    url: "./src/assets/wortel.png",
+    category: "herbs",
+    price: 12850,
+  },
+  {
+    name: "seafood lokal / local seafoods",
+    url: "./src/assets/wortel.png",
+    category: "seafoods",
+    price: 12850,
+  },
+  {
+    name: "Wortel lokal / local carrot",
+    url: "./src/assets/wortel.png",
+    category: "others",
+    price: 12850,
+  },
+  {
+    name: "Sayur lokal / local vegies",
+    url: "./src/assets/wortel.png",
+    category: "vegetables",
+    price: 12850,
+  },
+  {
+    name: "Sayur lokal / local vegies",
+    url: "./src/assets/wortel.png",
+    category: "vegetables",
+    price: 12850,
+  },
+  {
+    name: "Sayur lokal / local vegies",
+    url: "./src/assets/wortel.png",
+    category: "vegetables",
+    price: 12850,
+  },
+  {
+    name: "Wortel lokal / local carrot",
+    url: "./src/assets/wortel.png",
+    category: "others",
+    price: 12850,
+  },
+];
+const categories = [
+  "All",
+  "Vegetables",
+  "Fruits",
+  "Meats",
+  "Herbs",
+  "Seafoods",
+  "Others",
+];
+
+const productsPerPage = ref(12);
+const selectedCategory = ref("all");
+const searchQuery = ref("");
+const currentPage = ref(1);
+
+function filteredProducts() {
+  let filteredProducts = products;
+  if (selectedCategory.value !== "all") {
+    filteredProducts = filteredProducts.filter(
+      (product) => product.category === selectedCategory.value
+    );
+  }
+  if (searchQuery.value !== "") {
+    filteredProducts = filteredProducts.filter((product) =>
+      product.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+    );
+  }
+  return filteredProducts;
+}
+function totalPages() {
+  return Math.ceil(filteredProducts().length / productsPerPage.value);
+}
+function paginatedProducts() {
+  const startIndex = (currentPage.value - 1) * productsPerPage.value;
+  return filteredProducts().slice(
+    startIndex,
+    startIndex + productsPerPage.value
+  );
+}
+</script>
+
+<template>
+  <header class="bg-bannerp relative z-50">
+    <nav class="border-gray-200 bg-black bg-opacity-50 backdrop-blur-sm">
+      <div
+        class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+      >
+        <router-link to="/" class="flex items-center">
+          <img
+            src="../assets/kitaanterinlogo4.png"
+            class="h-12 mr-3"
+            alt="Kita Anterin Logo"
+          />
+        </router-link>
+        <div class="flex items-center">
+          <button
+            id="hamBtn"
+            type="button"
+            class="text-sm rounded-sm md:hidden"
+            @click="myFunction"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+        <div id="nav" class="hidden md:block w-full md:w-auto">
+          <ul
+            class="flex flex-col font-medium m-4 rounded-lg bg-black bg-opacity-50 md:bg-opacity-0 backdrop-blur-sm md:backdrop-blur-none md:flex-row md:space-x-8 md:m-0 md:border-0 md:bg-transparent"
+          >
+            <li v-for="nav in navs">
+              <router-link
+                :to="nav.url"
+                class="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0"
+                >{{ nav.teks }}</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <!-- banner -->
+  <div class="bg-bannerp h-banner flex items-center justify-center relative -z-50">
+    <div
+      class="text-3xl sm:text-5xl lg:text-6xl text-white bg-black bg-opacity-20 p-8 rounded-full fixed -translate-y-24"
+    >
+      OUR PRODUCTS
+    </div>
+  </div>
+
+  <!-- content -->
+  <div class="bg-white mx-auto max-w-screen-xl pt-8 md:pt-16">
+    <div class="bg-greenka p-2 rounded-full mx-4 md:mx-8 mb-4 md:mb-8">
+      <div
+        class="ml-auto rounded-full py-2 px-4 w-48 sm:w-64 bg-white flex items-center gap-1"
+      >
+        <img src="../assets/searchicon.svg" class="w-5" alt="searchicon" />
+        <input
+          type="text"
+          class="focus:outline-none w-full"
+          placeholder="Search...."
+          v-model="searchQuery"
+        />
+      </div>
+    </div>
+    <div class="mx-6 sm:mx-12 md:mx-24">
+      <!-- category -->
+      <div
+        class="flex gap-2 md:gap-4 my-4 md:my-8 overflow-x-scroll md:overflow-x-auto text-sm sm:text-base"
+      >
+        <button
+          class="categorybtn p-1 sm:p-2 px-4 sm:px-8 md:px-0 border-black border-2 cursor-pointer rounded-full grow text-center hover:bg-greenka2 hover:text-white hover:border-greenka2 transition"
+          v-for="category in categories"
+          @click="
+            selectedCategory = category.toLowerCase();
+            currentPage = 1;
+          "
+          :class="category"
+        >
+          {{ category }}
+        </button>
+      </div>
+      <div
+        class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8 pb-8 md:pb-16"
+      >
+        <a
+          href="#"
+          class="rounded-3xl overflow-hidden flex flex-col p-4 gap-8 shadow-lg shadow-slate-400 hover:shadow transition"
+          v-for="(product, index) in paginatedProducts()"
+        >
+          <img
+            src="../assets/kitaanterinlogo.png"
+            class="w-12 mx-auto"
+            alt="logokitaanterin"
+          />
+          <div>
+            <img
+              :src="product.url"
+              class="mx-auto"
+              :alt="'gambar ' + product.name"
+            />
+          </div>
+          <div>
+            <div class="font-medium sm:text-xl">
+              {{
+                product.price.toLocaleString("id-ID", {
+                  style: "currency",
+                  currency: "IDR",
+                })
+              }}
+            </div>
+            <div class="text-sm sm:text-base">{{ product.name }}</div>
+          </div>
+        </a>
+      </div>
+      <div class="m-auto sm:ml-auto w-fit flex gap-4 sm:gap-8 pb-12 md:pb-24 items-center">
+        <button
+          @click="currentPage--"
+          :disabled="currentPage === 1"
+          class="text-white text-sm sm:text-base font-medium p-2 px-4 sm:px-8 bg-greenka3 rounded-xl"
+        >
+          &lt;pre
+        </button>
+        <div>{{ currentPage }}/{{ totalPages() }}</div>
+        <button
+          @click="currentPage++"
+          :disabled="currentPage === totalPages()"
+          class="text-white text-sm sm:text-base font-medium p-2 px-4 sm:px-8 bg-greenka3 rounded-xl"
+        >
+          next&gt;
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.bg-bannerp {
+  background: url(../assets/bannerp.png) center/cover no-repeat fixed;
+}
+</style>
---
+++ src/components/Tips.vue
@@ -0,0 +1,204 @@
+<script setup>
+import { ref } from "vue";
+import { reactive } from "vue";
+
+const navs = [
+  {
+    teks: "HOME",
+    url: "/",
+  },
+  {
+    teks: "SHOP",
+    url: "/Shop",
+  },
+  {
+    teks: "TIPS",
+    url: "/Tips",
+  },
+];
+const specialTips = [
+  {
+    judul: "Anti sakit perut club1",
+    urlGambar: "./src/assets/specialtips01.png",
+  },
+  {
+    judul: "Anti sakit perut club2",
+    urlGambar: "./src/assets/tips1.png",
+  },
+  {
+    judul: "Anti sakit perut club3",
+    urlGambar: "./src/assets/tips2.png",
+  },
+  {
+    judul: "Anti sakit perut club4",
+    urlGambar: "./src/assets/tips3.png",
+  },
+  {
+    judul: "Anti sakit perut club",
+    urlGambar: "./src/assets/specialtips01.png",
+  },
+];
+const currentST = ref(1);
+const tips = [
+  {
+    judul: "menyimpan daging agar tahan lama",
+    urlGambar: "./src/assets/tips1.png",
+  },
+  {
+    judul: "vegetarian snack from terong",
+    urlGambar: "./src/assets/tips2.png",
+  },
+  {
+    judul: "manfaat sawi putih untuk anak",
+    urlGambar: "./src/assets/tips3.png",
+  },
+  {
+    judul: "tip1 jadi kurus dalam 1 detik",
+    urlGambar: "./src/assets/tips1.png",
+  },
+  {
+    judul: "tip2 jadi kurus dalam 1 detik",
+    urlGambar: "./src/assets/tips2.png",
+  },
+  {
+    judul: "tip3 jadi kurus dalam 1 detik",
+    urlGambar: "./src/assets/tips3.png",
+  },
+];
+const topTips = tips.slice(0, 3);
+
+function myFunction() {
+  var element = document.getElementById("hamBtn");
+  element.classList.toggle("close");
+  var element = document.getElementById("nav");
+  element.classList.toggle("open");
+}
+
+// setInterval(() => {
+//   if (currentST.value === specialTips.length) {
+//     currentST.value = 1;
+//   }else{
+//     currentST.value++;
+//   }
+// }, 3000)
+// function changeCurrentST(){
+// var activeDot = document.querySelector('#dot'+currentST.value)
+//   var element = document.getElementById(activeDot);
+//   element.classList.toggle("active");
+// }
+</script>
+
+<template>
+  <nav class="bg-greenka relative z-50 shadow-md">
+    <div class="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
+      <div
+        class="text-white w-full md:w-1/4 font-medium mt-4 md:mt-0 text-2xl md:text-xl order-3 md:order-none text-center"
+      >
+        <hr class="h-0.5 bg-white block md:hidden" />
+        TIPS FROM US
+        <hr class="h-0.5 bg-white block md:hidden" />
+      </div>
+      <router-link
+        to="/"
+        class="flex items-center mx-auto w-1/2 md:w-auto md:justify-center"
+      >
+        <img
+          src="../assets/kitaanterinlogo5.png"
+          class="h-12 md:h-20"
+          alt="Kita Anterin Logo"
+        />
+      </router-link>
+      <div
+        class="flex items-center justify-end md:justify-center w-1/2 md:hidden"
+      >
+        <button
+          id="hamBtn"
+          type="button"
+          class="text-sm rounded-sm"
+          @click="myFunction"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+
+      <div id="nav" class="hidden md:block w-full md:w-1/4" style="top: 7rem">
+        <ul
+          class="flex flex-col justify-end font-medium m-4 rounded-lg bg-black bg-opacity-50 md:bg-opacity-0 backdrop-blur-sm md:backdrop-blur-none md:flex-row md:space-x-8 md:m-0 md:border-0 md:bg-transparent"
+        >
+          <li v-for="nav in navs">
+            <router-link
+              :to="nav.url"
+              class="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0"
+              >{{ nav.teks }}</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div>
+    <div class="m-6 md:m-12 mb-12 md:mb-24 md:flex md:gap-8 md:items-center">
+      <div class="relative mb-4 md:mb-0">
+        <div class="">
+          <img
+            :src="specialTips[currentST - 1].urlGambar"
+            :alt="'gambar' + specialTips[currentST - 1].judul"
+            class="h-120 ratio-3/2 object-cover"
+          />
+        </div>
+        <div
+          class="text-white font-semibold absolute left-0 bottom-0 right-0 p-4 md:p-8 bg-black bg-opacity-50"
+        >
+          <div class="text-2xl">Edisi puasa</div>
+          <a href="" class="text-xl">{{ specialTips[currentST - 1].judul }}</a>
+        </div>
+        <div
+          class="absolute flex gap-1 bottom-3 right-3 md:bottom-6 md:right-6"
+        >
+          <div
+            v-for="(s, index) in specialTips"
+            :key="index"
+            :id="'dot'+(index + 1)"
+            class="w-2 h-2 bg-white rounded-full"
+            @click="currentST = index + 1"
+          ></div>
+        </div>
+        <button
+          class="absolute text-7xl top-1/2 -translate-y-1/2 left-4 bg-black bg-opacity-50 text-white grid place-items-center rounded-full p-2"
+          @click="currentST--"
+          :disabled="currentST === 1"
+        >
+          <img src="../assets/toleft.svg" class="w-6 h-6 pr-2" alt="" />
+        </button>
+        <button
+          class="absolute text-7xl top-1/2 -translate-y-1/2 right-4 bg-black bg-opacity-50 text-white grid place-items-center rounded-full p-2"
+          @click="currentST++"
+          :disabled="currentST === specialTips.length"
+        >
+          <img src="../assets/toright.svg" class="w-6 h-6 pl-2" alt="" />
+        </button>
+      </div>
+      <div class="h-120 flex flex-col gap-4 md:gap-8">
+        <div
+          class="flex items-center gap-3 md:gap-6 overflow-hidden"
+          v-for="tip in topTips"
+        >
+          <img
+            :src="tip.urlGambar"
+            class="object-cover h-28 sm:h-36 md:h-32 lg:h-36 ratio-5/4"
+            :alt="'gambar ' + tip.judul"
+          />
+
+          <div class="font-medium sm:font-semibold">
+            <div class="xs:text-xl sm:text-2xl md:text-lg">Tips!!</div>
+            <a href="#" class="xs:text-lg sm:text-xl md:text-base">{{
+              tip.judul
+            }}</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
